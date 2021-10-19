48c26cabeaf3a8bd2530782c0b10802da8569a32
user <user10@gmail.com>
2021-10-19 13:45:50 +0200

    Добавьте файлы проекта.

---
+++ im gay/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace rabota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.1 Сложный уровень
+            Console.WriteLine("Введите x:");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите y:");
+            double y = double.Parse(Console.ReadLine());
+            if (x <= 1 && x >= -1 && y >= x && y <= 1) Console.WriteLine("yes");
+            else Console.WriteLine("no");
+
+        }
+           
+    }
+}
